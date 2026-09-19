9b857650f254ac6c301fbd361e7755d19a9b4633
Your Name <you@example.com>
2026-09-18 21:21:27 -0400

    button works with the selection menu

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const generateButton = document.querySelector("#generateButton");
+const resourceSelect = document.querySelector("#resourceSelect");
 
 generateButton.addEventListener("click", function() {
-    console.log("Clicked!");
+   const selectedResouce = resourceSelect.value;
+
+    console.log("Selected resource:", selectedResouce);
 });
