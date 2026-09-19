c6cc849e6176ffea4315c90617319ec11f4a60b2
Your Name <you@example.com>
2026-09-18 23:16:08 -0400

    XP gain now gives level progression

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const woodcuttingXP = document.querySelector("#woodcuttingXP");
 const miningXP = document.querySelector("#miningXP");
 const fishingXP = document.querySelector("#fishingXP");
 
+const woodcuttingLevel = document.querySelector("#woodcutting");
+const miningLevel = document.querySelector("#mining");
+const fishingLevel = document.querySelector("#fishing");
+
 generateButton.addEventListener("click", function() {
    const selectedResource = resourceSelect.value;
 
@@ -20,14 +24,26 @@ generateButton.addEventListener("click", function() {
 
    if (selectedResource === "wood") {
     woodcuttingXP.textContent = Number(woodcuttingXP.textContent) + 1;
+
+    if (Number(woodcuttingXP.textContent) % 10 === 0) {
+        woodcuttingLevel.textContent = Number(woodcuttingLevel.textContent) + 1;
+    }
    }
 
    if (selectedResource === "copper") {
     miningXP.textContent = Number(miningXP.textContent) + 1;
+
+    if (Number(miningXP.textContent) % 10 === 0) {
+        miningLevel.textContent = Number(miningLevel.textContent) + 1;
+    }
    }
 
    if (selectedResource === "fish") {
     fishingXP.textContent = Number(fishingXP.textContent) + 1;
+    
+    if (Number(fishingXP.textContent) % 10 === 0) {
+        fishingLevel.textContent = Number(fishingLevel.textContent) + 1;
+    }
    }
 
     console.log("Generated:", selectedResource);
